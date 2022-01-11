f8a7fa16cdf2cf30d045de7f93e9f24057a2417f
Prasanna RV <prasannavelmurugan0200@gmail.com>
2022-01-11 21:41:11 +0530

    chanegs

---
+++ public/javascript/pop-up.js
@@ -0,0 +1,8 @@
+const crossButton = document.querySelector('.cross-button');
+const popUpSection = document.querySelector('.pop-up-section');
+
+
+
+crossButton.addEventListener('click', () => {
+    popUpSection.style.display = 'none';
+});
