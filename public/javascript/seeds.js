b842df1711eb448c5029e05ede10d7a78df5c11a
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2022-01-11 18:35:25 +0530

    Initial commit with ourprojects

---
+++ public/javascript/seeds.js
@@ -0,0 +1,23 @@
+const para = [
+    "Cras1 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras2 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras3 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras4 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras5 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras6 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras7 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+    "Cras8 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
+  ];
+  
+  const heading = [
+    "Project 1",
+    "Project 2",
+    "Project 3",
+    "Project 4",
+    "Project 5",
+    "Project 6",
+    "Project 7",
+    "Project 8",
+  ];
+
+  const image = [];
