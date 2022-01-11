9c6f0a3bbfff36064ffc9f70ea87c70dab71de02
Prasanna RV <prasannavelmurugan0200@gmail.com>
2022-01-11 21:57:32 +0530

    chnages

--- public/javascript/pop-up.js
+++ public/javascript/pop-up.js
@@ -1,8 +1,13 @@
 const crossButton = document.querySelector('.cross-button');
 const popUpSection = document.querySelector('.pop-up-section');
 
+function noScroll() {
+    window.scrollTo(0, 0);
+}
 
+window.addEventListener('scroll', noScroll);
 
 crossButton.addEventListener('click', () => {
     popUpSection.style.display = 'none';
+    window.removeEventListener('scroll', noScroll);
 });
