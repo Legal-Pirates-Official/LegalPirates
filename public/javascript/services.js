3e3e5b0cdb2681f6c0ca60a3e1e56cdc189d1af9
Prasanna RV <prasannavelmurugan0200@gmail.com>
2022-01-12 16:41:05 +0530

    changes

---
+++ public/javascript/services.js
@@ -0,0 +1,7 @@
+const mobileAppBox = document.querySelector('.mobile-app-box');
+const wordpressBox = document.querySelector('.wordpress-box');
+const webDevBox = document.querySelector('.web-dev-box');
+
+const mobileAppColumn = document.querySelector('.mobile-app-column');
+const wordpressColumn = document.querySelector('.wordpress-column');
+const webDevColumn = document.querySelector('.web-dev-column');
