766569fb40d6b9312cbae8e8e442209d78510579
Lohith <srklohith05@gmail.com>
2022-01-12 22:37:16 +0530

    Our projects

--- public/javascript/seeds.js
+++ public/javascript/seeds.js
@@ -1,23 +1,57 @@
 const para = [
-    "Cras1 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras2 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras3 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras4 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras5 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras6 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras7 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-    "Cras8 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.",
-  ];
-  
-  const heading = [
-    "Project 1",
-    "Project 2",
-    "Project 3",
-    "Project 4",
-    "Project 5",
-    "Project 6",
-    "Project 7",
-    "Project 8",
-  ];
+	'Cras1 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras2 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras3 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras4 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras5 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras6 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras7 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.',
+	'Cras8 pretium metus pulvinar ultricies auctor. In varius purus blandit sem mollis tristique curba sed lorem vel ligula pulvinar porttitor. Proin sit amet mauris eleifend amet, lacus asfd ligula viverra sagittis at at odio aliquam erat volutpat suspendisse sagittis lorem sum bear only.'
+];
 
-  const image = [];
+const heading = [
+	'Project 1',
+	'Project 2',
+	'Project 3',
+	'Project 4',
+	'Project 5',
+	'Project 6',
+	'Project 7',
+	'Project 8'
+];
+
+const sliderImages = [
+	[
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001897/projects/Trekidoo/trekidoo1_egcsid.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001897/projects/Trekidoo/trekidoo2_xmavkt.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001899/projects/Trekidoo/trekidoo3_x6bwul.png'
+	],
+	[
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001895/projects/LP-Aircraft-company/lp-aircraft-company-1_zstksm.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/LP-Aircraft-company/lp-aircraft-company-3_qwvqft.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001895/projects/LP-Aircraft-company/lp-aircraft-company-2_wjp5eq.png'
+	],
+	[
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001895/projects/Infotech/infotech-1_b2qr47.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/Infotech/infotech-2_svplwz.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001895/projects/Infotech/infotech-3_hm3ufx.png'
+	],
+	[
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642006215/projects/Legal-Coders/legal-coders-2_totfv2.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642006216/projects/Legal-Coders/legal-coders-1_fyukff.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642006214/projects/Legal-Coders/legal-coders-3_yucmjy.png'
+	],
+	[
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/TEDx/tedx-1_kijwcq.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/TEDx/tedx-2_yv9o88.png',
+		'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/TEDx/tedx-3_rg5cbf.png'
+	]
+];
+
+const image = [
+	'https://res.cloudinary.com/legal-pirates/image/upload/v1642001897/projects/Trekidoo/trekidoo1_egcsid.png',
+	'https://res.cloudinary.com/legal-pirates/image/upload/v1642001895/projects/LP-Aircraft-company/lp-aircraft-company-2_wjp5eq.png',
+	'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/Infotech/infotech-4_etazjo.png',
+	'https://res.cloudinary.com/legal-pirates/image/upload/v1642006215/projects/Legal-Coders/legal-coders-2_totfv2.png',
+	'https://res.cloudinary.com/legal-pirates/image/upload/v1642001896/projects/TEDx/tedx-1_kijwcq.png'
+];
