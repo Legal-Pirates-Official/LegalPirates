cfaa46738238a1ad592cbc528b688b994eb2284e
Lohith <srklohith05@gmail.com>
2022-01-13 11:12:01 +0530

    Changes

--- public/javascript/three.js
+++ public/javascript/three.js
@@ -4,26 +4,26 @@ import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/js
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
 );
 camera.position.z = 2.5;
 
 const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas: document.querySelector('#hero-left')
+	antialias: true,
+	canvas: document.querySelector('#hero-left')
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const heroLeft = new THREE.Mesh(
-    new THREE.SphereGeometry(),
-    new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load('public/images/network.jpg')
-    })
+	new THREE.SphereGeometry(),
+	new THREE.MeshBasicMaterial({
+		map: new THREE.TextureLoader().load('public/images/network.jpg')
+	})
 );
 scene.add(heroLeft);
 
@@ -32,30 +32,28 @@ group.add(heroLeft);
 scene.add(group);
 
 const mouse = {
-    x: 0,
-    y: 0
+	x: 0,
+	y: 0
 };
 if (innerWidth >= 700) {
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableZoom = false;
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.enableZoom = false;
 
-    document
-        .querySelector('#hero-left')
-        .addEventListener('mousemove', (e) => {
-            (mouse.x = (e.clientX / innerWidth) * 2 - 1),
-                (mouse.y = (e.clientY / innerHeight) * 2 + 1);
-        });
+	document.querySelector('#hero-left').addEventListener('mousemove', (e) => {
+		(mouse.x = (e.clientX / innerWidth) * 2 - 1),
+			(mouse.y = (e.clientY / innerHeight) * 2 + 1);
+	});
 }
 function animate() {
-    requestAnimationFrame(animate);
-    heroLeft.rotation.y += 0.001;
-    // controls.update();
-    gsap.to(group.rotation, {
-        x: mouse.x * 0.5,
-        y: -mouse.y * 0.5,
-        duration: 2
-    });
-    renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+	heroLeft.rotation.y += 0.001;
+	// controls.update();
+	gsap.to(group.rotation, {
+		x: mouse.x * 0.5,
+		y: -mouse.y * 0.5,
+		duration: 2
+	});
+	renderer.render(scene, camera);
 }
 
 animate();
